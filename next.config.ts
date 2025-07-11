c69ddd7b16127414dd8fc824e92355fba9bec2ee
j10c <blyb1739@gmail.com>
2025-07-12 00:37:22 +0800

    chore(build): bundle static for crx

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: process.env.CRX_BUILD === "1" ? "export" : undefined
+};
 
 export default nextConfig;
