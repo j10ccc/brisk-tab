f39dce90934f0604462abccb1f920df18b955dbf
j10c <blyb1739@gmail.com>
2025-07-12 01:08:20 +0800

    chore(build): transform crx bundle product
    Fight against the understore requirement for crx souce.

---
+++ scripts/next-out-transform.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import { globSync } from "glob";
+
+console.info("[script] Start transform next out files...");
+
+const files = globSync("out/**/*.html");
+
+files.forEach((file) => {
+  const content = fs.readFileSync(file, "utf-8");
+  const modifiedContent = content.replace(/\/_next/g, "./next");
+  fs.writeFileSync(file, modifiedContent, "utf-8");
+});
+
+const sourcePath = "out/_next";
+const destinationPath = "out/next";
+
+try {
+  fs.renameSync(sourcePath, destinationPath);
+  console.info('[script] Renamed "_next" directory to "next" successfully.');
+} catch (err) {
+  console.error('[script] Failed to rename "_next" directory to "next".', err);
+}
