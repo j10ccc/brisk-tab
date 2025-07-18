3264db7e3372d701f6e955ae821b361d6859dcc2
j10c <blyb1739@gmail.com>
2025-07-19 00:33:31 +0800

    fix(ui): stop event propagation when input

--- src/ui/input/index.tsx
+++ src/ui/input/index.tsx
@@ -1,4 +1,4 @@
-import { Ref } from "react";
+import { KeyboardEventHandler, Ref } from "react";
 
 import styles from "./index.module.css";
 
@@ -12,10 +12,22 @@ export interface InputProps
 export default function Input(props: InputProps) {
   const { ref, label, field, ...otherProps } = props;
 
+  const handleKeyDown: KeyboardEventHandler<HTMLInputElement> = (e) => {
+    e.stopPropagation();
+    props.onKeyDown?.(e);
+  };
+
   return (
     <div className={styles.container}>
       {label ? <label htmlFor={field}>{label}</label> : null}
-      <input ref={ref} id={field} name={field} type="text" {...otherProps} />
+      <input
+        ref={ref}
+        id={field}
+        name={field}
+        type="text"
+        {...otherProps}
+        onKeyDown={handleKeyDown}
+      />
     </div>
   );
 }
